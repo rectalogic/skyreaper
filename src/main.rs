ad431db81bb61a6d6b684c2b6feb162a37f455d5
Andrew Wason <rectalogic@rectalogic.com>
2024-12-16 11:31:41 -0500

    position relative to viewport

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ fn main() {
     App::new()
         .add_plugins((DefaultPlugins, PhysicsPlugins::default()))
         .add_systems(Startup, setup)
+        .add_systems(Update, update.run_if(run_once))
         .run();
 }
 
@@ -55,4 +56,47 @@ fn setup(
         Camera3d::default(),
         Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Dir3::Y),
     ));
+}
+
+fn update(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    camera_query: Single<(&Camera, &GlobalTransform)>,
+) {
+    let (camera, camera_transform) = *camera_query;
+    /*
+    if let Some(world) = camera.ndc_to_world(
+        camera_transform,
+        Vec3 {
+            x: 0.,
+            y: 1.,
+            z: 0.01,
+        },
+    ) {
+        commands.spawn((
+            Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
+            MeshMaterial3d(materials.add(Color::WHITE)),
+            Transform::from_translation(world),
+        ));
+    }
+    */
+
+    if let Some(viewport) = camera.logical_viewport_rect() {
+        if let Ok(ray) = camera.viewport_to_world(
+            camera_transform,
+            Vec2 {
+                x: viewport.max.x / 2.0,
+                y: viewport.min.y,
+            },
+        ) {
+            // Project along frustum to origin (i.e. distance of camera in Z)
+            let world = ray.get_point(camera_transform.translation().z);
+            commands.spawn((
+                Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
+                MeshMaterial3d(materials.add(Color::WHITE)),
+                Transform::from_translation(world),
+            ));
+        }
+    }
 }
